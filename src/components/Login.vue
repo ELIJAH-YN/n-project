1080e7c133c32031ce2d4b25cc24178568838528
Ian <lucusyuan46@gmail.com>
2020-10-18 01:40:52 +0800

    vuetify login and register

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card width="450" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">登入</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field 
+          label="Email"
+          prepend-icon="mdi-email"
+        />
+        <v-text-field 
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          prepend-icon="mdi-lock" 
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+        />
+      </v-form>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-btn color="success">註冊</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="info">登入</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+
+    data: () => ({
+      showPassword: false
+    }),
+  }
+</script>
---
+++ src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <h1 class="display-1">註冊</h1>
+        </v-card-title>
+        <v-card-text>
+            <v-form>
+                <v-text-field
+                  label="名稱"
+                  prepend-icon="mdi-account-circle" 
+                />
+                <v-text-field 
+                  label="電子郵件"
+                  prepend-icon="mdi-email"
+                />
+                <v-text-field 
+                  :type="showPassword ? 'text' : 'password'"
+                  label="密碼"
+                  prepend-icon="mdi-lock"
+                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="showPassword = !showPassword"
+                />
+            </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+            <v-btn color="success">送出</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'Reigster',
+    data: () => ({
+        showPassword: false
+    }),
+}
+</script>
